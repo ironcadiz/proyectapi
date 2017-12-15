c966073b159afd7942f3291e0b4187894a6676fa
Andres <afcadiz@uc.cl>
2017-12-15 12:54:12 -0300

    proyect init

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,18 @@
 const config = {
   default: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USERNAME || 'proyectapiadmin',
+    password: process.env.DB_PASSWORD || '123123',
     dialect: process.env.DB_DIALECT || "postgres",
     database: process.env.DB_NAME,
     host: process.env.DB_HOST || "127.0.0.1",
   },
   development: {
     extend: "default",
-    database: "boilerplate_dev",
+    database: "proyectapi_dev",
   },
   test: {
     extend: "default",
-    database: "boilerplate_test",
+    database: "proyectapi_test",
   },
   production: {
     extend: "default",
