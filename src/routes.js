9f524ecde93f7f6e4830b72b1301b6c332960b07
Andres <afcadiz@uc.cl>
2017-12-17 13:57:15 -0300

    added get communities route

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ const index = require("./routes/index")
 const hello = require("./routes/hello")
 const auth = require("./routes/auth")
 const user = require("./routes/user")
+const community = require("./routes/community")
 
 router.use("/auth", auth.routes())
 
 router.use("/", index.routes())
 router.use("/hello", hello.routes())
 router.use("/users", user.routes())
+router.use("/communities", community.routes())
 
 module.exports = router
