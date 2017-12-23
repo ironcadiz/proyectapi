88b7763f27454eadc5928880b4c45b70542e7c7d
Gabriel Ulloa <gsulloa@uc.cl>
2017-12-23 20:05:45 -0300

    Middleware to obtain community, used for events routes

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,37 +1,10 @@
 const KoaRouter = require("koa-router")
 const auth = require("../middlewares/authentication")
+const getCommunity = require("../middlewares/getCommunity")
 
 const router = new KoaRouter()
 
 router.use(auth)
-router.post("event", "/", async ctx => {
-  const voluntaries = await ctx.state.currentUser.getVoluntaries()
-  const voluntary = voluntaries[0]
-  const community = await voluntary.getCommunity()
-  const { description, name, start } = ctx.request.body
-  const ev = await ctx.orm.Event.build({
-    name,
-    description,
-    start: new Date(start),
-    communityId: community.id,
-  }).save()
-  ctx.response.status = 201
-  ctx.body = ev
-})
-
-router.get("event", "/", async ctx => {
-  const voluntaries = await ctx.state.currentUser.getVoluntaries()
-  const voluntary = voluntaries[0]
-  if (voluntary) {
-    const community = await voluntary.getCommunity()
-    let events = await community.getEvents()
-    ctx.response.status = 200
-    ctx.body = { events }
-  } else {
-    ctx.body = "user has no associated voluntary"
-    ctx.status = 422
-  }
-})
 
 router.get("event", "/all", async ctx => {
   if (ctx.state.currentUser.isAdmin) {
@@ -45,9 +18,45 @@ router.get("event", "/all", async ctx => {
 })
 
 router.get("event", "/date/:date", async ctx => {
-  const events = await ctx.orm.Event.findAll({ where: { start: { $between: [new Date(ctx.params.date), new Date(`${ctx.params.date}T23:59:59.99`)] } } })
+  const events = await ctx.orm.Event.findAll({
+    where: {
+      start: {
+        $between: [
+          new Date(ctx.params.date),
+          new Date(`${ctx.params.date}T23:59:59.99`)
+        ],
+      },
+    },
+  })
   ctx.response.status = 200
   ctx.body = { events }
+})
+
+/* after using getCommunity, if user has community, it will be in ctx.state.authData.community */
+router.use(getCommunity)
+router.post("event", "/", async ctx => {
+  const community = ctx.state.authData.community
+  const { description, name, start } = ctx.request.body
+  const ev = await ctx.orm.Event.build({
+    name,
+    description,
+    start: new Date(start),
+    communityId: community.id,
+  }).save()
+  ctx.response.status = 201
+  ctx.body = ev
+})
+
+router.get("event", "/", async ctx => {
+  const community = ctx.state.authData.community
+  if (community) {
+    let events = await community.getEvents()
+    ctx.response.status = 200
+    ctx.body = { events }
+  } else {
+    ctx.body = "user has no community"
+    ctx.status = 422
+  }
 })
 
 module.exports = router
