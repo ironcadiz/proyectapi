2a90bd33055c5f0ec89c40f4e97097a78c90f665
ironcadiz <afcadiz@uc.cl>
2017-12-31 12:07:36 -0300

    hotifx (#12)

--- src/routes/event.js
+++ src/routes/event.js
@@ -48,14 +48,27 @@ router.post("event", "/", async ctx => {
 })
 
 router.get("event", "/", async ctx => {
-  const community = ctx.state.authData.community
-  if (community) {
+  const voluntaries = await ctx.state.currentUser.getVoluntaries()
+  const voluntary = voluntaries[0]
+  if (voluntary) {
+    const community = await voluntary.getCommunity()
     let events = await community.getEvents()
     ctx.response.status = 200
     ctx.body = { events }
   } else {
-    ctx.body = "user has no community"
+    ctx.body = "user has no associated voluntary"
     ctx.status = 422
+  }
+})
+
+router.get("event", "/all", async ctx => {
+  if (ctx.state.currentUser.isAdmin) {
+    let events = await ctx.orm.Event.findAll()
+    ctx.response.status = 200
+    ctx.body = { events }
+  } else {
+    ctx.body = "user isnt admin"
+    ctx.status = 401
   }
 })
 
