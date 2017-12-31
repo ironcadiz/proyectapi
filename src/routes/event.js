23513f9a3ca67ed89f5dfbbf367a6f0dc9a626e4
ironcadiz <afcadiz@uc.cl>
2017-12-31 13:06:58 -0300

    Dev (#14)
    * First models (#2)
    
    * added voluntary model and test rout on /hello
    
    * added community model
    
    * Added community model and seeds
    
    * added get communities route
    
    * Witchmail (#3)
    
    * added witchmail model and migration
    
    * Aded create witchmail route
    
    * added get witchmail and set seen
    
    * Reports (#4)
    
    * Added user info on login
    
    * Added manual model and migration
    
    * Added section seeds
    
    * added get manuals and get sections routes.
    
    * added report model
    
    * Added create report route
    
    * Added event models migration and routes. (#5)
    
    * Agenda (#7)
    
    * Added event models migration and routes.
    
    * added get all events route
    
    * hotifx (#9)
    
    * Session data (#11)
    
    * Encoding more data in the session
    
    * Middleware to obtain community, used for events routes
    
    * hotifx (#12)
    
    * added batch route (#13)

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,13 +1,41 @@
 const KoaRouter = require("koa-router")
 const auth = require("../middlewares/authentication")
+const getCommunity = require("../middlewares/getCommunity")
 
 const router = new KoaRouter()
 
 router.use(auth)
+
+router.get("event", "/all", async ctx => {
+  if (ctx.state.currentUser.isAdmin) {
+    let events = await ctx.orm.Event.findAll()
+    ctx.response.status = 200
+    ctx.body = { events }
+  } else {
+    ctx.response.status = 401
+    ctx.body = "Unauthorized!"
+  }
+})
+
+router.get("event", "/date/:date", async ctx => {
+  const events = await ctx.orm.Event.findAll({
+    where: {
+      start: {
+        $between: [
+          new Date(ctx.params.date),
+          new Date(`${ctx.params.date}T23:59:59.99`)
+        ],
+      },
+    },
+  })
+  ctx.response.status = 200
+  ctx.body = { events }
+})
+
+/* after using getCommunity, if user has community, it will be in ctx.state.authData.community */
+router.use(getCommunity)
 router.post("event", "/", async ctx => {
-  const voluntaries = await ctx.state.currentUser.getVoluntaries()
-  const voluntary = voluntaries[0]
-  const community = await voluntary.getCommunity()
+  const community = ctx.state.authData.community
   const { description, name, start } = ctx.request.body
   const ev = await ctx.orm.Event.build({
     name,
@@ -50,15 +78,9 @@ router.get("event", "/all", async ctx => {
     ctx.response.status = 200
     ctx.body = { events }
   } else {
-    ctx.response.status = 401
-    ctx.body = "Unauthorized!"
+    ctx.body = "user isnt admin"
+    ctx.status = 401
   }
-})
-
-router.get("event", "/date/:date", async ctx => {
-  const events = await ctx.orm.Event.findAll({ where: { start: { $between: [new Date(ctx.params.date), new Date(`${ctx.params.date}T23:59:59.99`)] } } })
-  ctx.response.status = 200
-  ctx.body = { events }
 })
 
 module.exports = router
